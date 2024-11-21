c97a287f7aa786778d7cface68a8aab753d48a75
Linards <lincis20@inbox.lv>
2024-11-21 19:12:12 +0200

    style: changed font to Inter

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
     ],
     theme: {
         extend: {
+            fontFamily: {
+                sans: ["var(--font-inter)"],
+            },
             colors: {
                 background: "var(--background)",
                 foreground: "var(--foreground)",
