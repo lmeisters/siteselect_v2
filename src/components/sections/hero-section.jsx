62d1589c6b846eaf153422f1a5a69bdf94e428a6
Linards <lincis20@inbox.lv>
2024-11-22 17:28:32 +0200

    style: applied layout tweaks for improved design

--- src/components/sections/hero-section.jsx
+++ src/components/sections/hero-section.jsx
@@ -3,12 +3,11 @@ export function HeroSection() {
         <section className="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8 pt-40 pb-16 flex flex-col items-center justify-center">
             <div className="w-full text-center">
                 <h1 className="text-[clamp(4rem,15vw,16rem)] font-black leading-none tracking-tighter mx-auto">
-                    SiteSelect
+                    SiteSelect.
                 </h1>
                 <div className="mx-auto max-w-xl mt-32">
                     <p className="text-md leading-tight font-medium">
                         Discover the Web&apos;s Most Innovative Design Galleries
-                        -
                     </p>
                     <p className="text-md leading-tight font-medium">
                         Categorized and Filtered for Instant Creative
